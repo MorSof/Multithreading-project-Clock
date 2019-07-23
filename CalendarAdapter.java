3265f19c6ebf468888a7199871c12e16ba8e2185
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2019-07-23 10:26:29 +0300

    Create CalendarAdapter.java

---
+++ CalendarAdapter.java
@@ -0,0 +1,19 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalendarAdapter {
+
+	private Calendar calendar = new GregorianCalendar();
+
+	public int getHour() {
+		return calendar.get(Calendar.HOUR_OF_DAY);
+	}
+
+	public int getMinute() {
+		return calendar.get(Calendar.MINUTE);
+	}
+
+	public int getSecond() {
+		return calendar.get(Calendar.SECOND);
+	}
+}
